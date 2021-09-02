dec2f5c91caa892eb9dee7d6fccdf243fe71d966
Artur Michałowski <artur.michalowski93@gmail.com>
2021-09-02 18:29:24 +0200

    Just onlu 1 test fail! ;)

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -199,21 +199,19 @@ void Controller::handlePause(const PauseInd& pauseInd) {
 }
 
 void Controller::receive(std::unique_ptr<Event> e) {
-    if (e->getMessageId() == PauseInd::MESSAGE_ID) {
-        handlePause(*static_cast<EventT<PauseInd> const&>(*e));
-    }
-
     switch (e->getMessageId()) {
     case TimeoutInd::MESSAGE_ID:
-        return handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
+        if (!isPaused) {
+            return handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
+        }
     case DirectionInd::MESSAGE_ID:
         return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
     case FoodInd::MESSAGE_ID:
         return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
     case FoodResp::MESSAGE_ID:
         return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
-    // case PauseInd::MESSAGE_ID:
-    //     return handlePause(*static_cast<EventT<PauseInd> const&>(*e));
+    case PauseInd::MESSAGE_ID:
+        return handlePause(*static_cast<EventT<PauseInd> const&>(*e));
     default:
         throw UnexpectedEventException();
     };
